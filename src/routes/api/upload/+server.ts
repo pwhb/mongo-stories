067fd7abde940bfe1a0892318495d407a13727ec
Phone Waiyam Hein <phonewaiyamhein@ayainnovation.com>
2023-01-19 17:54:21 +0630

    modified

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -2,7 +2,10 @@ import Song from '$lib/models/song';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-	// const body = await request.json();
+	const body = await request.body?.getReader();
 	console.log('api called');
+    
+    
+	console.log('body', body);
 	return json({ test: 'hello friend' });
 };
