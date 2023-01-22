bbf3c3d6427ce0884a33464060a2084d52c43f7d
pwhb <pwhbdev@gmail.com>
2023-01-23 00:08:21 +0630

    made at home

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import { getContext } from "$lib/controllers/contexts";
 import { getSocials } from "$lib/controllers/socials";
 
 
-export async function load({}) {
+export async function load() {
     const socials = await getSocials()
     const context = await getContext()
     console.log("Loaded data!!!")
--- src/routes/manage/+page.svelte
+++ src/routes/manage/+page.svelte
@@ -1,17 +1,42 @@
+<script>
+	import { each } from 'svelte/internal';
+
+	const menu = [
+		{
+			title: 'Songs',
+			submenu: [
+				{
+					url: 'songs',
+					name: 'songs'
+				},
+				{
+					url: 'songs/add',
+					name: 'add'
+				}
+			]
+		},
+		{
+			title: 'Stories',
+			submenu: [
+				{
+					url: 'songs/add',
+					name: 'home'
+				}
+			]
+		}
+	];
+	console.log(menu)
+</script>
 
 <div class="hero min-h-[70vh]">
-
-<ul class="menu bg-base-100 w-56 mx-auto my-48 rounded-box">
-	<li class="menu-title">
-		<span>Category</span>
-	</li>
-	<li><a></a></li>
-	<li><a>Item 2</a></li>
-	<li class="menu-title">
-		<span>Category</span>
-	</li>
-	<li><a>Item 1</a></li>
-	<li><a>Item 2</a></li>
-</ul>
-
+	<ul class="menu bg-base-100 w-56 mx-auto my-48 rounded-box">
+		{#each menu as { title, submenu }}
+			<li class="menu-title">
+				<span>{title}</span>
+			</li>
+			{#each submenu as { url, name }}
+				<li><a href={`manage/${url}`}>{name}</a></li>
+			{/each}
+		{/each}
+	</ul>
 </div>
